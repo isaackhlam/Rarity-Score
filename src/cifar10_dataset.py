3aefa83dff9f6763497b0d61b82c916818de8d31
isaackhlam <isaac.lamkh@gmail.com>
2023-10-17 21:00:58 +0800

    fix: update normalization value for custom wrapper

--- src/cifar10_dataset.py
+++ src/cifar10_dataset.py
@@ -4,7 +4,7 @@ from torchvision import datasets, transforms
 from torch.utils.data import Dataset
 
 transform = transforms.Compose([
-    transforms.ToTensor(), transforms.Normalize(mean=0.5, std=0.5)
+    transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 class CIFAR10Dataset(Dataset):
