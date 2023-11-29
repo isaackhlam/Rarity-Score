003279e8aa18e40aee4f6f100e4f5fcc200efb3a
ivan00105 <johnwong14793@gmail.com>
2023-11-29 09:37:25 +0800

    rerun resnet with gan

--- src/resnet_gan_cifar10.py
+++ src/resnet_gan_cifar10.py
@@ -34,6 +34,7 @@ class ImageDataset(Dataset):
 
 
 transform = transforms.Compose([
+    transforms.Resize((299, 299)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
